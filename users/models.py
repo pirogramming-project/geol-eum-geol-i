e879c4a2de97a6101e63c302ae28cea907ca2605
Kimgyuilli <rlarbdlf222@naver.com>
2025-01-31 03:06:09 +0900

    [feat] 로그인 이메일로 변경

--- users/models.py
+++ users/models.py
@@ -1,34 +1,30 @@
+import uuid
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, user_id, email, password=None, nickname=None, **extra_fields):
+    def create_user(self, email, password=None, nickname=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
-        if not user_id:
-            raise ValueError('The User ID field must be set')
         if not nickname:
             raise ValueError('The Nickname field must be set')
 
         email = self.normalize_email(email)
-        user = self.model(user_id=user_id, email=email, nickname=nickname, **extra_fields)
+        extra_fields.setdefault('user_id', str(uuid.uuid4())[:8])  # 🔥 user_id 자동 생성
+
+        user = self.model(email=email, nickname=nickname, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, user_id, email, password=None, nickname=None, **extra_fields):
+    def create_superuser(self, email, password=None, nickname=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
-
-        return self.create_user(user_id, email, password, nickname, **extra_fields)
+        return self.create_user(email, password, nickname, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    user_id = models.CharField(max_length=100, unique=True)
+    user_id = models.CharField(max_length=100, unique=True, editable=False, default=str(uuid.uuid4())[:8])  # 🔥 자동 생성
     email = models.EmailField(unique=True, blank=False, null=False)
     nickname = models.CharField(max_length=50, blank=False, null=False)
     is_active = models.BooleanField(default=True)
@@ -37,8 +33,8 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
-    USERNAME_FIELD = 'user_id'
-    REQUIRED_FIELDS = ['email', 'nickname'] 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nickname']
 
     def __str__(self):
-        return self.user_id
+        return self.email
