a8d82e6c03ed54d117a5bde766f0dcfcf74974a3
parkgunwook <ss944161269441@gmail.com>
2025-02-01 01:23:38 +0900

    [feat]: 지도 기반 위치 탐지, 추천 페이지 연결 추가

--- config/urls.py
+++ config/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('users.urls')),
-    #path('', include('course.urls'))
+    #path('', include('users.urls')),
+    path('', include('course.urls'))
 ]
 
 if settings.DEBUG:  
