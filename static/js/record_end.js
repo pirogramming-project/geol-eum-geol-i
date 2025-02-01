e5c02592fc245272e62bd9c8a121430777128c65
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-02-02 01:23:52 +0900

    [fix]: JS 시간 카운트 및 데이터 전송하기

--- static/js/record_end.js
+++ static/js/record_end.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     let path = JSON.parse(sessionStorage.getItem("path") || "[]");
     let watchID;
+    let startTime = new Date(sessionStorage.getItem("startTime"));
+    let totalDistance = 0;
+    let caloriesBurned = 0;
+
+    const showDistance = document.querySelector(".record__e_total_dist");
+    const showCalories = document.querySelector(".record__e_total_cal");
+    const showTime = document.querySelector(".record__e_total_time");
 
     console.log("저장된 경로 데이터: ", path);
 
@@ -28,6 +35,8 @@ document.addEventListener("DOMContentLoaded", function() {
                         path.push(newPosition);
                         sessionStorage.setItem("path", JSON.stringify(path));
                         console.log("실시간 좌표: ", newPosition);
+
+                        updateDisNCal();
                     },
                     (error) => console.error("실시간 좌표 수집 불가:", error),
                     { enableHighAccuracy: true, maximumAge: 1000, timeout: 5000}
@@ -38,45 +47,63 @@ document.addEventListener("DOMContentLoaded", function() {
         );
     }
 
+    function updateDisNCal() {
+        totalDistance = calcDistance(path);
+        caloriesBurned = totalDistance * 50; // 식 수정필요
+
+        showDistance.textContent = `얼마걸음: ${totalDistance.toFixed(2)}km`;
+        showCalories.textContent = `총 소비칼로리: ${caloriesBurned}kcal`;
+    }
+
+    function updateTime() {
+        let now = new Date();
+        let durationSec = Math.floor((now-startTime)/1000); // 초 단위변환
+        let hours = Math.floor(durationSec/3600);
+        let min = Math.floor((durationSec % 3600)/60);
+        let sec = durationSec % 60;
+
+        showTime.textContent = `${hours.toString().padStart(2, "0")}:${min.toString().padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
+    }
+
+    function calcDistance(coords) {
+        let totalDistance = 0;
+        function haversine(lat1, lon1, lat2, lon2) {
+            const R = 6371; // 지구 반지름
+            const dLat = (lat2-lat1) * Math.PI / 180;
+            const dLon = (lon2-lon1) * Math.PI / 180;
+            const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180)
+                * Math.sin(dLon / 2) * Math.sin(dLon/2);
+            const c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            return R*c;
+        }
+        for (let i=1; i<coords.length; i++) {
+            totalDistance += haversine(
+                coords[i-1].latitude, coords[i-1].longitude,
+                coords[i].latitude, coords[i].longitude
+            );
+        }
+        return totalDistance;
+    }
+
+    let timeUpdate = setInterval(updateTime, 1000);
+
     document.getElementById("stopBtn").addEventListener("click", function() {
         let endTime = new Date().toISOString(); // ISO 형식 저장
-        let startTime = sessionStorage.getItem("startTime");
 
         navigator.geolocation.clearWatch(watchID);
+        clearInterval(timeUpdate);
 
-        let totalDistance = calcDistance(path);
-        let durationSec = (new Date(endTime) - new Date(startTime)) / 1000; // 초 단위로 변환
+        let durationSec = Math.floor((new Date(endTime) - startTime) / 1000); // 초 단위로 변환
         let minutes = durationSec / 60; // 분 단위
-        let pace = 0;
+        let pace = 0.00;
         if (totalDistance > 0) {
             pace = (minutes/totalDistance).toFixed(2);
-        }
-        let caloriesBurned = totalDistance * 50; // 식 수정해야 함
-
-        function calcDistance(coords) {
-            let totalDistance = 0;
-            function haversine(lat1, lon1, lat2, lon2) {
-                const R = 6371; // 지구 반지름
-                const dLat = (lat2-lat1) * Math.PI / 180;
-                const dLon = (lon2-lon1) * Math.PI / 180;
-                const a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
-                    + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180)
-                    * Math.sin(dLon / 2) * Math.sin(dLon/2);
-                const c = 2*Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-                return R*c;
-            }
-            for (let i=1; i<coords.length; i++) {
-                totalDistance += haversine(
-                    coords[i-1].latitude, coords[i-1].longitude,
-                    coords[i].latitude, coords[i].longitude
-                );
-            }
-            return totalDistance;
         }
 
         // API에 보낼 데이터 구조
         let daily_record = {
-            start_time: startTime,
+            start_time: startTime.toISOString(),
             end_time: endTime,
             distance: totalDistance.toFixed(2),
             time: durationSec,
