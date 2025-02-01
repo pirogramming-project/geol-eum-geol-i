df55319ba8b580da3f8435c2a19a227ddc8d0875
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-02-01 10:15:24 +0900

    [add]: record_end(기록종료) 페이지까지 완료

--- record/urls.py
+++ record/urls.py
@@ -5,4 +5,6 @@ app_name = 'record'
 
 urlpatterns = [
     path('main_page/', main_view, name='main'),
+    path('start/', record_start, name='record_start'),
+    path('stop/', record_stop, name='record_stop'),
 ]
--- record/views.py
+++ record/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def main_view(request):
     return render(request, 'main/landing.html')
+
+def record_start(request):
+    return render(request, 'record/record_start.html')
+
+def record_stop(request):
+    return render(request, 'record/record_end.html')
