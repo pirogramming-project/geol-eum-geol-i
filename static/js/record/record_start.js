6d7349f53d3661ce453b779d7c5ea153740bd94d
NamKyeongMin <kyeongmin35@gachon.ac.kr>
2025-02-06 16:17:42 +0900

    [add]: 걸음기록 중지버튼(bottleBtn) 기능 추가

--- static/js/record/record_start.js
+++ static/js/record/record_start.js
@@ -1,6 +1,3 @@
 document.getElementById("startBtn").addEventListener("click", function() {
-    sessionStorage.clear(); // 기존 경로 데이터 초기화
-
-    sessionStorage.setItem("path", JSON.stringify([]));
     window.location.href="/record/ready/";
 });
